94882f849c9b66c6de3756259a2000f08dd557f7
Chamilka Mihiraj <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2025-03-08 10:27:30 +0530

    backend -new

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-});
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+});
+
+mongoose.connect('mongodb+srv://chamilkamihiraj26:chamilka123@compitition.abvci.mongodb.net/Database?retryWrites=true&w=majority&appName=Compitition')
+.then(() => {
+    console.log('Connected to Database');
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000')
+    });
+})
+.catch((err) => {
+    console.log('Unable to connect : ',err);
 });
