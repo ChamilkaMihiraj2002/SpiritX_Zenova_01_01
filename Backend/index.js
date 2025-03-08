9db51b4b5cc03c4dfdc4602084cc8939de9bb22c
Chamilka Mihiraj <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2025-03-08 10:37:43 +0530

    backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 const app = express()
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
-mongoose.connect('mongodb+srv://chamilkamihiraj26:chamilka123@compitition.abvci.mongodb.net/Database?retryWrites=true&w=majority&appName=Compitition')
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
     console.log('Connected to Database');
-    app.listen(3000, () => {
-        console.log('Server is running on port 3000')
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`)
     });
 })
 .catch((err) => {
